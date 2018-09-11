ce0aaf8e789d8fcc7a3522642f4c93e4e99c7f8a
tylersul02 <42984907+tylersul02@users.noreply.github.com>
2018-09-11 09:37:22 -0700

    Update vars2.py

--- vars2.py
+++ vars2.py
@@ -1,1 +1,9 @@
+# vars2.py trs
+a = 10 # integer (int)
+b = -5 # integer (int)
+s2 = 1.414 # float
+s = "This is a string of character" # string
+s3 = 'How about this' # string
+print (a,b,s2,s,s3)
+print ("the square root of 2 is"
 
